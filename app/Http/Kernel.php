36695b94480e4310d6d2a448cad46a913b052b14
Juan Isidro Saucedo Mena <juansaucedo.mena@gmail.com>
2018-05-02 12:25:55 -0500

    Modificación de middleware para evitar CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -33,7 +34,8 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+//            \App\Http\Middleware\VerifyCsrfToken::class,
+            Cors::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
