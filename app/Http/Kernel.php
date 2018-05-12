2a2bc6c6de2c5949f291a5bab72ea4e277566e83
Juan Isidro Saucedo Mena <juansaucedo.mena@gmail.com>
2018-05-11 19:45:19 -0500

    Agregado sistema de CORS de este blog: https://www.sshakil.com/blog/article/7/Enabling-CORS-for-Laravel-requested-by-Angular

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -35,7 +36,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
 //            \App\Http\Middleware\VerifyCsrfToken::class,
-            Cors::class,
+//            Cors::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,14 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');
 
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        if (!$request->isMethod('options')) {
+            return $next($request);
+        }
 
     }
 }
